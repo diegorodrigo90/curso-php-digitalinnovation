456d1dce19162306f6d0f382e991ea232c3b4d90
DIEGO RODRIGO DA SILVA <diegors@unicamp.br>
2020-07-27 14:56:20 -0300

    Initial commit

---
+++ web/app/test/FooTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppTest;
+
+use App\Foo;
+use PHPUnit\Framework\TestCase;
+
+class FooTest extends TestCase
+{
+    public function testGetName()
+    {
+        $foo = new Foo();
+        $this->assertEquals($foo->getName(), 'Nginx PHP MySQL');
+    }
+}
---
+++ web/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+include '../app/vendor/autoload.php';
+$foo = new App\Foo();
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Docker <?php echo $foo->getName(); ?></title>
+    </head>
+    <body>
+        <h1>Docker <?php echo $foo->getName(); ?></h1>
+    </body>
+</html>
