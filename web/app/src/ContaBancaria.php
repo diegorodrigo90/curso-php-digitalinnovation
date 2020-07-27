4829c9ab780c348f4ea81434e09e19284c4f3824
DIEGO RODRIGO DA SILVA <diegors@unicamp.br>
2020-07-27 15:54:09 -0300

    class ContaBancaria

---
+++ web/app/src/ContaBancaria.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class ContaBancaria
+{
+    //public - private - protected
+    public $banco;
+    public $nomeTitular;
+    public $numeroAgencia;
+    public $numeroConta;
+    public $saldo;
+
+
+
+}
